e3096fde60cf699b61eb5d3cfe419d3ba578cfed
Bayubotz5 <96644261+Bayubotz5@users.noreply.github.com>
2021-12-29 13:38:55 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,14 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *ANIME LOVERS*
+_https://chat.whatsapp.com/ITCvKcfieJm0D9XAsTPJUU_
+2. *ANIME LOVERS*
+_https://chat.whatsapp.com/ITCvKcfieJm0D9XAsTPJUU_
+3. *BAYU BOTZ WHATSAPP*
+_https://chat.whatsapp.com/CG8MUUI461d0OCaEwuSryE_
+4. *BAYU BOTZ WHATSAPP* 
+_https://chat.whatsapp.com/CG8MUUI461d0OCaEwuSryE_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
